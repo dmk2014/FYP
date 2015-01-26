2be94418eaf62732a11741bb15e1ffaf986369ce
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-01-26 20:58:01 +0000

    Working on Facial Recogniton algorithm - training database

--- Octave/image_read.m
+++ Octave/image_read.m
@@ -6,5 +6,26 @@ image = imread("~/Desktop/FYP/YaleTrainingDatabase/yaleB01/yaleB01_P00A-005E-10.
 #Write image to disk
 imwrite(image, "~/Desktop/FYP/Octave/testimage.png", "png");
 
+#A = A'
+imageTranspose  = image';
+
 #Converting image to column vector
-colVector = reshape(image,32256,1);
+colVector = reshape(imageTranspose,32256,1);
+
+#Above is for one image
+#We need to read each image from the training set and find the column vectors in turn
+#Transpose step is to ensure correct ordering
+#e.g [1,2,3
+#     4,5,6]
+#becomes [1,2,3,4,5,6] column vector, as oppsed to Octace reshape() default of [1,4,2,5,3,6]
+
+trainingDatabase = readdir("~/Desktop/FYP/YaleTrainingDatabase");
+
+for i=1:length(trainingDatabase)
+  if(isdir(i))
+    i
+  endif
+  
+  #dir = trainingDatabase(i)
+  #dir
+endfor
