d2d9f7ad52823c4d3c4719cc3b5ceef6e4aa4102
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-01-30 17:43:36 +0000

    Octave: calculation of Covariance matrix, Eigenvectors and Eigenvalues

--- Octave/reductionAndCovariance.m
+++ Octave/reductionAndCovariance.m
@@ -2,9 +2,19 @@
 reducedFaces = matrixOfColumnVectors - averageValues;
 
 #Calculate covariance matrix
-c = double(reducedFaces')*double(reducedFaces);
+C = cov(reducedFaces);
 
-#This will fail, ridiculously inefficient
+#C = A * A' will fail, ridiculously inefficient
 #Trying to calculate n^2 * n^2 matrix = 32256*32256
+#Solution is to use an MxM numbers, where M is number of training images (2432)
+#Octave's cov function will handle this process for us
 
-#Solution is to use an M numbers, where M is number of training images (2432)
+#Next step is PCA to calculate Eigenvectors
+#Achieved via decomposition of the covariance matrix C
+[V,D] = eig(C);
+
+#where
+#V is eigenvectors
+#D is diagonal matrix of eigenvalues of C
+
+#Sort the columns of the eigenvector matrix V and eigenvalue matrix D in order of decreasing eigenvalue
