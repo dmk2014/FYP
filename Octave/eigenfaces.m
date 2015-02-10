d1331475c4d7734c6871632badfdace3887ae478
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-02-10 23:39:46 +0000

    Octave: eigenface script cleanup. Code now uses SVD function to perform PCA. Much better results than previously used EIG function

--- Octave/eigenfaces.m
+++ Octave/eigenfaces.m
@@ -1,10 +1,8 @@
-#Load database
 #M = loadYaleTrainingDatabase("C:/Users/t00157675/Desktop/fyp/FYP/YaleTrainingDatabase/");
 M = loadYaleTrainingDatabase("~/Desktop/FYP/YaleTrainingDatabase/");
 
 #Reduce matrix using mean
-#averageFace = calculateMean(M);
-averageFace = mean(M,2); 
+averageFace = calculateMean(M);
 reducedFaces = reduceFaces(M,averageFace);
 
 #Calculate covariance matrix
@@ -13,7 +11,6 @@ reducedFaces = reduceFaces(M,averageFace);
 #Solution is to use an MxM numbers, where M is number of training images (2432), or C = A' * A
 #Octave's cov() function will handle this process for us
 #C = cov(reducedFaces);
-C = reducedFaces' * reducedFaces;
 
 #Next step is PCA - calculates Eigenvectors and Eigenvalues
 #Achieved via decomposition of the covariance matrix C
@@ -23,43 +20,23 @@ C = reducedFaces' * reducedFaces;
 
 #D(i,i) is the eigenvalue relative to V(:,i)
 #D(1,1) for V(:,1) -> every row for 1st column, so entire first column
-[V D] = eig(C);
+#[V D] = eig(C);
 
 #Sort the columns of the eigenvector matrix V and eigenvalue matrix D in order of decreasing eigenvalue
-[D,i] = sort(diag(D), 'descend');
-V = V(:,i);
+#[D,i] = sort(diag(D), 'descend');
+#V = V(:,i);
 
+#SVD is an alternate method of performing PCA
+#Results here appear vastly more accurate
+[U S V] = svd(reducedFaces,"econ");
 
-#[U S V] = svd(reducedFaces,"econ");
+U = U(:,1:50); #Select first 50 eigenfaces
 
-#U = U(:,1:50);
-
-#for h=1:columns(V)
-#  V(:,h) = normalize(V(:,h),0,255);
-#endfor
-
-######
-#[uselessVariable,permutation]=sort(diag(D),'descend');
-#D=D(permutation,permutation);
-#V=V(:,permutation);
-######
-
-#Retrieve k higher dimensional eigenvectors & normalize data [0-255]
-#U2 = getHigherDimensionalEigenvectors(V,reducedFaces,50);
-U3 = reducedFaces * V;
-
-#for h=1:columns(U)
-#  U(:,h) = normalize(U(:,h),0,255);
-#endfor
+#Retrieve k higher dimensional eigenvectors
+#U = getHigherDimensionalEigenvectors(V,reducedFaces,50);
+#U = reducedFaces * V;
 
 #Represent each image in terms of the k eignenfaces
 #Find a weight vector for each training set image
 #Wj = Uj' * ReducedFacesi
 weights = findWeights(reducedFaces, U);
-
-project = findWeights(reducedFaces(:,1),U);
-
-#Normalize weights
-#for h=1:columns(weights)
-#  weights(:,h) = normalize(weights(:,h),0,255);
-#endfor
