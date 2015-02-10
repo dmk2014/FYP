09204987fd2154bdd5cb9419b40fcacdbbea994b
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-02-10 23:35:47 +0000

    Octave: working on solution to reconstruction problem

--- Octave/eigenfaces.m
+++ Octave/eigenfaces.m
@@ -12,7 +12,8 @@ reducedFaces = reduceFaces(M,averageFace);
 #Trying to calculate n^2 * n^2 matrix = 32256*32256
 #Solution is to use an MxM numbers, where M is number of training images (2432), or C = A' * A
 #Octave's cov() function will handle this process for us
-C = cov(reducedFaces);
+#C = cov(reducedFaces);
+C = reducedFaces' * reducedFaces;
 
 #Next step is PCA - calculates Eigenvectors and Eigenvalues
 #Achieved via decomposition of the covariance matrix C
@@ -22,11 +23,16 @@ C = cov(reducedFaces);
 
 #D(i,i) is the eigenvalue relative to V(:,i)
 #D(1,1) for V(:,1) -> every row for 1st column, so entire first column
-[V,D] = eig(C);
+[V D] = eig(C);
 
 #Sort the columns of the eigenvector matrix V and eigenvalue matrix D in order of decreasing eigenvalue
 [D,i] = sort(diag(D), 'descend');
 V = V(:,i);
+
+
+#[U S V] = svd(reducedFaces,"econ");
+
+#U = U(:,1:50);
 
 #for h=1:columns(V)
 #  V(:,h) = normalize(V(:,h),0,255);
@@ -39,18 +45,21 @@ V = V(:,i);
 ######
 
 #Retrieve k higher dimensional eigenvectors & normalize data [0-255]
-U = getHigherDimensionalEigenvectors(V,reducedFaces,100);
+#U2 = getHigherDimensionalEigenvectors(V,reducedFaces,50);
+U3 = reducedFaces * V;
 
-for h=1:columns(U)
-  U(:,h) = normalize(U(:,h),0,255);
-endfor
+#for h=1:columns(U)
+#  U(:,h) = normalize(U(:,h),0,255);
+#endfor
 
 #Represent each image in terms of the k eignenfaces
 #Find a weight vector for each training set image
 #Wj = Uj' * ReducedFacesi
 weights = findWeights(reducedFaces, U);
 
+project = findWeights(reducedFaces(:,1),U);
+
 #Normalize weights
-for h=1:columns(weights)
-  weights(:,h) = normalize(weights(:,h),0,255);
-endfor
+#for h=1:columns(weights)
+#  weights(:,h) = normalize(weights(:,h),0,255);
+#endfor
