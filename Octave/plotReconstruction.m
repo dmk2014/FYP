deef0500ec2953690246b14e1a529b97619f3511
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-02-11 18:29:30 +0000

    Octave: function to plot original face side by side with its reconstruction

--- Octave/plotReconstruction.m
+++ Octave/plotReconstruction.m
@@ -1,9 +1,11 @@
 function plotReconstruction(originalFace, reconstructedFace, k)
   subplot(1,2,1);
+  originalFace = reshape(originalFace,192,168); #TODO: remove hard-coded values
   imshow(originalFace, []);
   title("Original Face");
   
-  sunplot(1,2,2);
+  subplot(1,2,2);
+  reconstructedFace = reshape(reconstructedFace,192,168);
   imshow(reconstructedFace, []);
-  title("Reconstruction (Using %k eigenfaces)",k);
+  title(sprintf("Reconstruction (Using %i eigenfaces)",k));
 endfunction
