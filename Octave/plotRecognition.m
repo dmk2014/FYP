4d9a6add60bc31578a07aeb8edba153a9b8617b9
Darren <darren.m.keane@students.ittralee.ie>
2015-02-12 16:14:43 +0000

    Octave: plot unknown face and recognised face side-by-side

---
+++ Octave/plotRecognition.m
@@ -0,0 +1,16 @@
+function plotRecognition(M,unknownFace,idxFound)
+  #M = face set
+  #unknownFace = face to be recognised
+  #idxFound = index of closest match in database
+  subplot(1,2,1);
+  unknownFace = reshape(unknownFace,192,168); #TODO: remove hard-coded values
+  imshow(unknownFace, []);
+  title("Unknown Face");
+  
+  closestMatch = M(:,idxFound);
+  closestMatch = reshape(closestMatch,192,168);
+  
+  subplot(1,2,2);
+  imshow(closestMatch, []);
+  title("Closest Match in Database");
+endfunction
