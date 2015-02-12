c379ca380f7284983a2933cf62c8da2653c3cb21
Darren <darren.m.keane@students.ittralee.ie>
2015-02-12 16:13:52 +0000

    Octave: functionised testRecogniser. Finds nearest match based on euclidean distance

--- Octave/testRecogniser.m
+++ Octave/testRecogniser.m
@@ -1,14 +1,20 @@
-#Proejecting New Face
-image = double(imread("~/Desktop/yaleB01_P00A+000E+00.pgm"));
+function [image distance idx] = testRecogniser(U,weights,averageFace,facePath)
+  #Projecting New Face
+  image = double(imread(facePath));
+  #"~/Desktop/yaleB01_P00A+000E+00.pgm"
+  
+  image = reshape(image,rows(image) * columns(image),1);
 
-image = reshape(image,rows(image) * columns(image),1);
+  weightOfUnknownFace = projectFace(U,image,averageFace);
 
-weightOfUnknownFace = projectFace(U,image,averageFace);
-
-#Euclidian Distance
-#Loop through weights and find smallest distance
-#ED = norm(W - W, 2); #use 2-norm approach
-
-for i=1:columns(weights)
-  ED(1,i) = norm(weights(:,i) - weightOfUnknownFace, 2);
-endfor
+  #Euclidian Distance
+  #Loop through weights and find smallest distance
+  #ED = norm(W - W, 2); #use 2-norm approach
+  
+  #ED from between unknown face and each face
+  for i=1:columns(weights)
+    ED(1,i) = norm(weights(:,i) - weightOfUnknownFace, 2);
+  endfor
+  
+  [distance idx] = min(ED);
+endfunction
