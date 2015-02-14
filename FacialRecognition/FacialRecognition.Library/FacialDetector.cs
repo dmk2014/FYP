18a74294303bd1e7a00691750672b870e9003adf
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-02-14 12:56:38 +0000

    FacialRecognition: detection implemented with EmguCV

--- FacialRecognition/FacialRecognition.Library/FacialDetector.cs
+++ FacialRecognition/FacialRecognition.Library/FacialDetector.cs
@@ -1,12 +1,22 @@
 ﻿using System.Drawing;
 
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+
 namespace FacialRecognition.Library
 {
     public class FacialDetector
     {
-        public void DetectFaces(Bitmap image)
+        public Rectangle[] DetectFaces(Bitmap image)
         {
-            //TODO
+            var _emguImage = new Image<Gray, byte>(image);
+
+            var _classifier = new CascadeClassifier(@"C:\Emgu\emgucv-windows-universal-cuda 2.4.10.1940\bin\haarcascade_frontalface_default.xml");
+
+            var _faces = _classifier.DetectMultiScale(_emguImage, 1.1, 3, Size.Empty, Size.Empty);
+
+            return _faces;
         }
     }
 }
