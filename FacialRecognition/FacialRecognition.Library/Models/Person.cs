8c89ee3a38f385a2a0c31df5a82380381c285971
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-02-15 00:17:49 +0000

    FacialRecognition: barebones Person model

---
+++ FacialRecognition/FacialRecognition.Library/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FacialRecognition.Library.Models
+{
+    class Person
+    {
+        public int _id;
+        public String Forename { get; set; }
+        public String Surname { get; set; }
+
+        //TODO
+        //Handle images and other required fields
+
+        public void SetID(int _id)
+        {
+            this._id = _id;
+        }
+    }
+}
