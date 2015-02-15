90d5414cc5e2c35f1b3fb4637630cab664cd3851
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-02-15 23:17:38 +0000

    FacialRecognition: integrating with Octave via Redis

---
+++ FacialRecognition/FacialRecognition.Library/OctaveInterface.cs
@@ -0,0 +1,45 @@
+﻿using ServiceStack.Redis;
+using System;
+
+namespace FacialRecognition.Library
+{
+    public class OctaveInterface
+    {
+        RedisClient c_Client;
+
+        public OctaveInterface(RedisClient Client)
+        {
+            //TODO
+            this.c_Client = Client;
+        }
+
+        public void SendRequest(OctaveMessage Type, String Data)
+        {
+            //TODO
+            var _transaction = new RedisTransaction(c_Client);
+            _transaction.QueueCommand(r => r.Set("facial.request.code", Type));
+            _transaction.QueueCommand(r => r.Set("facial.request.data", Data));
+
+            _transaction.Commit();
+        }
+
+        public void ReceiveResponse()
+        {
+            //TODO
+            //We need to allow Redis time to process request
+            //Maybe set a timer, pass timeout as argument?
+
+            do
+            {
+                var _responseCodeString = c_Client.Get("facial.response.code").ToString();
+                var _reponseCode = int.Parse(_responseCodeString);
+
+                if (_reponseCode != (int)OctaveMessage.NO_DATA)
+                {
+                    //TODO - parse response
+                    var _reponseData = c_Client.Get("facial.response.data").ToString();
+                }
+            } while (true);
+        }
+    }
+}
---
+++ FacialRecognition/FacialRecognition.Library/OctaveMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacialRecognition.Library
+{
+    public enum OctaveMessage
+    {
+        NO_DATA = 0,
+        REQUEST_REC = 100,
+        REQUEST_RELOAD = 200,
+        REQUEST_SAVE = 300,
+        RESPONSE_OK = 100,
+        RESPONSE_FAIL = 200
+    }
+}
