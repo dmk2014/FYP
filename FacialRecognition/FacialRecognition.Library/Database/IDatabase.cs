d97c98fea7d8c1bf524df237489b9bc8ba68c33b
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-02-15 00:18:34 +0000

    FacialRecognition: barebones database interface

---
+++ FacialRecognition/FacialRecognition.Library/Database/IDatabase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacialRecognition.Library.Database
+{
+    interface IDatabase
+    {
+        //TODO
+        //Define all DB functionality required
+        //Store a person
+        //Update a person
+        //Retrieve a person
+        //Retrieve all persons
+
+        public Boolean Store(FacialRecognition.Library.Models.Person Person);
+        public Boolean Update(FacialRecognition.Library.Models.Person Person);
+
+        public FacialRecognition.Library.Models.Person Retrieve();
+        public FacialRecognition.Library.Models.Person RetrieveAll();
+    }
+}
