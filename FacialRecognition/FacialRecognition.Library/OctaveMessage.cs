c0f63fd9e02f1d875281d1c9086d70d41c2fb6c6
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-02-16 20:04:58 +0000

    FacialRecognition: object to pass an Octave message

--- FacialRecognition/FacialRecognition.Library/OctaveMessage.cs
+++ FacialRecognition/FacialRecognition.Library/OctaveMessage.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FacialRecognition.Library
 {
-    public enum OctaveMessage
+    public class OctaveMessage
     {
-        NO_DATA = 0,
-        REQUEST_REC = 100,
-        REQUEST_RELOAD = 200,
-        REQUEST_SAVE = 300,
-        RESPONSE_OK = 100,
-        RESPONSE_FAIL = 200
+        public OctaveMessageType Code { set; get; }
+        public String Data { set; get; }
+
+        public OctaveMessage(OctaveMessageType Code, String Data)
+        {
+            this.Code = Code;
+            this.Data = Data;
+        }
     }
 }
