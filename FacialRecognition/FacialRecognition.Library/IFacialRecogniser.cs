3030e45d879f4d54f8f26cb01338c04b171855d6
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-02-16 20:20:49 +0000

    FacialRecognition: defining generic recogniser functionality

---
+++ FacialRecognition/FacialRecognition.Library/IFacialRecogniser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacialRecognition.Library
+{
+    interface IFacialRecogniser
+    {
+        //TODO
+        //Define generic functionality to recognise a facial image
+
+        Models.Person ClassifyFace(System.Drawing.Bitmap FacialImage);
+    }
+}
