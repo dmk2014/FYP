fbe9ccce3ec2ff7f95a1770d7d7a65d3104c0712
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-02-16 20:25:32 +0000

    FacialRecognition: concrete OctaveRecogniser

---
+++ FacialRecognition/FacialRecognition.Library/OctaveRecogniser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacialRecognition.Library
+{
+    public class OctaveRecogniser : IFacialRecogniser
+    {
+        public Models.Person ClassifyFace(System.Drawing.Bitmap FacialImage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
