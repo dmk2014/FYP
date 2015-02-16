1648aa5654046f00965f559073536edf6da28014
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-02-16 20:05:22 +0000

    FacialRecognition: integrating with Octave

--- FacialRecognition/FacialRecognition.Library/OctaveInterface.cs
+++ FacialRecognition/FacialRecognition.Library/OctaveInterface.cs
@@ -1,45 +1,59 @@
 ﻿using ServiceStack.Redis;
 using System;
+using System.Diagnostics;
 
 namespace FacialRecognition.Library
 {
     public class OctaveInterface
     {
-        RedisClient c_Client;
+        private RedisClient c_Client;
 
         public OctaveInterface(RedisClient Client)
         {
-            //TODO
             this.c_Client = Client;
         }
 
-        public void SendRequest(OctaveMessage Type, String Data)
+        public void SetRedisClient(RedisClient Client)
         {
-            //TODO
+            this.c_Client = Client;
+        }
+
+        public Boolean SendRequest(OctaveMessageType Type, String Data)
+        {
             var _transaction = new RedisTransaction(c_Client);
             _transaction.QueueCommand(r => r.Set("facial.request.code", Type));
             _transaction.QueueCommand(r => r.Set("facial.request.data", Data));
 
-            _transaction.Commit();
+            return _transaction.Commit();
         }
 
-        public void ReceiveResponse()
+        public OctaveMessage ReceiveResponse(int Timeout)
         {
-            //TODO
-            //We need to allow Redis time to process request
-            //Maybe set a timer, pass timeout as argument?
+            OctaveMessage _response = null;
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
 
-            do
+            while (watch.ElapsedMilliseconds <= Timeout)
             {
                 var _responseCodeString = c_Client.Get("facial.response.code").ToString();
                 var _reponseCode = int.Parse(_responseCodeString);
 
-                if (_reponseCode != (int)OctaveMessage.NO_DATA)
+                if (_reponseCode != (int)OctaveMessageType.NO_DATA)
                 {
-                    //TODO - parse response
                     var _reponseData = c_Client.Get("facial.response.data").ToString();
+                    _response = new OctaveMessage((OctaveMessageType) _reponseCode, _reponseData);
+                    break;
                 }
-            } while (true);
+            }
+
+            if (_response == null)
+            {
+                throw new TimeoutException("A response was not received from Octave in the specified time (" + Timeout + "ms)");
+            }
+            else
+            {
+                return _response;
+            }
         }
     }
 }
