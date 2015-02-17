c2b05a16bc19a1a0850e4dc1ef14869b13d9d9f5
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-02-17 11:06:15 +0000

    FacialRecognition: _id field changed to String

--- FacialRecognition/FacialRecognition.Library/Models/Person.cs
+++ FacialRecognition/FacialRecognition.Library/Models/Person.cs
@@ -4,14 +4,14 @@ namespace FacialRecognition.Library.Models
 {
     public class Person
     {
-        public int _id;
+        public String _id;
         public String Forename { get; set; }
         public String Surname { get; set; }
 
         //TODO
         //Handle images and other required fields
 
-        public void SetID(int _id)
+        public void SetID(String _id)
         {
             this._id = _id;
         }
