2a5437ade24165ac30e13b42123205bb10c3362f
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-02-21 13:02:34 +0000

    FacialRecognition: skeleton for image normalisation

---
+++ FacialRecognition/FacialRecognition.Library/FacialImageNormaliser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace FacialRecognition.Library
+{
+    public abstract class FacialImageNormaliser
+    {
+        public Image NormaliseImage(Image SourceImage, int Width, int Height)
+        {
+            Image _normalisedImage;
+            //Resize
+            _normalisedImage = this.Resize(SourceImage, Width, Height);
+
+            //Convert to Grayscale
+            _normalisedImage = this.SetColormap(SourceImage);
+
+            throw new NotImplementedException();
+        }
+
+        public abstract Image Resize(Image Source, int Width, int Height);
+        public abstract Image SetColormap(Image Source);
+    }
+}
---
+++ FacialRecognition/FacialRecognition.Library/OctaveNormaliser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace FacialRecognition.Library
+{
+    public class OctaveNormaliser : FacialImageNormaliser
+    {
+        public override Image Resize(Image Source, int Width, int Height)
+        {
+            throw new NotImplementedException();
+        }
+        public override Image SetColormap(Image Source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
