30474a7d35abce17ccb6be585d1dee97b5a311d2
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-02-21 13:28:43 +0000

    FacialRecognition: concrete OctaveNormaliser

--- FacialRecognition/FacialRecognition.Library/OctaveNormaliser.cs
+++ FacialRecognition/FacialRecognition.Library/OctaveNormaliser.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 
 namespace FacialRecognition.Library
 {
@@ -11,11 +13,37 @@ namespace FacialRecognition.Library
     {
         public override Image Resize(Image Source, int Width, int Height)
         {
-            throw new NotImplementedException();
+            var _resizedImage = new Bitmap(Width, Height);
+            var _graphics = Graphics.FromImage(_resizedImage);
+
+            _graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            _graphics.DrawImage(Source, 0, 0, Width, Height);
+            _graphics.Dispose();
+
+            return _resizedImage;
         }
+
         public override Image SetColormap(Image Source)
         {
-            throw new NotImplementedException();
+            //Reference: http://tech.pro/tutorial/660/csharp-tutorial-convert-a-color-image-to-grayscale
+
+            var _originalImage = new Bitmap(Source);
+            var _result = new Bitmap(Source.Width, Source.Height, PixelFormat.Format16bppGrayScale);
+
+            for (int i = 0; i < Source.Width; i++)
+            {
+                for (int j = 0; j < Source.Height; j++)
+                {
+                    var _pixel = _originalImage.GetPixel(i, j);
+                    var _pixelGrayscale = (_pixel.R + _pixel.G + _pixel.B) / 3;
+                    var _pixelAsColor = Color.FromArgb(_pixelGrayscale, _pixelGrayscale, _pixelGrayscale);
+
+                    _result.SetPixel(i, j, _pixelAsColor);
+                }
+            }
+
+
+            return _result;
         }
     }
 }
