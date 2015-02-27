5f8c1dcdcfaaa174aab3eaec4c8113d5333f9309
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-02-27 19:18:41 +0000

    FacialRecogntion: concrete CouchDB skeleton

---
+++ FacialRecognition/FacialRecognition.Library/Database/CouchDatabase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FacialRecognition.Library.Database
+{
+    class CouchDatabase : IDatabase
+    {
+        public bool Store(Models.Person Person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Models.Person Person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Models.Person Retrieve(string ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Models.Person> RetrieveAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
