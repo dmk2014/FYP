280230aa8bdf81dfd0a66fb98ccf0942630274af
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-02-27 21:16:13 +0000

    FacialRecognition: unit testing CouchDB

---
+++ FacialRecognition/FacialRecognition.Test/CouchDB_CRUD_Test.cs
@@ -0,0 +1,30 @@
+﻿using FacialRecognition.Library.Database;
+using FacialRecognition.Library.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FacialRecognition.Test
+{
+    [TestClass]
+    public class CouchDB_CRUD_Test
+    {
+        IDatabase c_DB;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            c_DB = new CouchDatabase("localhost", 5984, "People");
+        }
+
+        [TestMethod]
+        public void TestStoreAPerson()
+        {
+            var _person = new Person();
+            _person._id = "testID";
+            _person.Forename = "Unit";
+            _person.Surname = "Test";
+
+            c_DB.Store(_person);
+        }
+    }
+}
---
+++ FacialRecognition/FacialRecognition.Test/CouchDB_Connection_Test.cs
@@ -0,0 +1,19 @@
+﻿using FacialRecognition.Library.Database;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FacialRecognition.Test
+{
+    [TestClass]
+    public class CouchDB_Connection_Test
+    {
+        /// <summary>
+        /// Test connectivity with CouchDB. Test should be passed
+        /// if no exception are thrown while connecting
+        /// </summary>
+        [TestMethod]
+        public void TestCouchConnection()
+        {
+            var _db = new CouchDatabase("localhost", 5984, "People");
+        }
+    }
+}
