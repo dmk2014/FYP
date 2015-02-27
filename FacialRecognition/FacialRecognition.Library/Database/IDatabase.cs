2ff1064ad5dff77bb86ae03d6e0088ce33ee738d
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-02-27 19:17:40 +0000

    FacialRecogntion: updated IDatabase

--- FacialRecognition/FacialRecognition.Library/Database/IDatabase.cs
+++ FacialRecognition/FacialRecognition.Library/Database/IDatabase.cs
@@ -1,24 +1,14 @@
-﻿using System;
+﻿using FacialRecognition.Library.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FacialRecognition.Library.Database
 {
-    interface IDatabase
+    public interface IDatabase
     {
-        //TODO
-        //Define all DB functionality required
-        //Store a person
-        //Update a person
-        //Retrieve a person
-        //Retrieve all persons
-
-        Boolean Store(FacialRecognition.Library.Models.Person Person);
-        Boolean Update(FacialRecognition.Library.Models.Person Person);
-
-        FacialRecognition.Library.Models.Person Retrieve();
-        FacialRecognition.Library.Models.Person RetrieveAll();
+        Boolean Store(Person Person);
+        Boolean Update(Person Person);
+        Person Retrieve(String ID);
+        List<Person> RetrieveAll();
     }
 }
