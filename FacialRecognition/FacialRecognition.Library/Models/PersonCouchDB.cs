d517f75e372acffe00bc20eef4f2790ed62f6499
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-02-28 19:20:29 +0000

    FacialRecognition: added Person model for CouchDB interaction

---
+++ FacialRecognition/FacialRecognition.Library/Models/PersonCouchDB.cs
@@ -0,0 +1,11 @@
+﻿using DreamSeat.Interfaces;
+
+namespace FacialRecognition.Library.Models
+{
+    public class PersonCouchDB : Person, ICouchDocument
+    {
+        public string Id { get; set; }
+
+        public string Rev { get; set; }
+    }
+}
