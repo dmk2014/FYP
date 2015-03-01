b343d0cdb878b8dd5dba7fa401f148eeee0602ab
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-03-01 18:28:55 +0000

    FacialRecognition: CouchDB unit tests

--- FacialRecognition/FacialRecognition.Test/CouchDB_CRUD_Test.cs
+++ FacialRecognition/FacialRecognition.Test/CouchDB_CRUD_Test.cs
@@ -43,6 +43,53 @@ namespace FacialRecognition.Test
         }
 
         [TestMethod]
+        public void TestUpdateAPerson()
+        {
+            var _person = new Person();
+            _person.Id = "update123";
+            _person.Forename = "Stack";
+            _person.Surname = "Overflow";
+
+            c_DB.Store(_person);
+
+            //Person stored. DB generates Rev. Now lets change the person and update them
+            _person.Surname = "Trace";
+            c_DB.Update(_person);
+
+            //Retrieve the item, and cast it so we can verify the revision number
+            var _result = (PersonCouchDB)c_DB.Retrieve(_person.Id);
+            Assert.IsTrue(_result.Rev.StartsWith("2"));
+        }
+
+        [TestMethod]
+        public void TestUpdateNonExisitingPersonCreatesANewDocument()
+        {
+            var _person = new Person();
+            _person.Id = "doesntexist";
+            _person.Forename = "Mike";
+            _person.Surname = "Fratello";
+
+            c_DB.Update(_person);
+
+            //Assert creation
+            var _result = c_DB.Retrieve(_person.Id);
+
+            Assert.AreEqual(_result.Id, _person.Id);
+            Assert.AreEqual(_result.Forename, _person.Forename);
+            Assert.AreEqual(_result.Surname, _person.Surname);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TestRetrieveNonExistingPersonCausesException()
+        {
+            var _person = new Person();
+            _person.Id = "abc123xyznotindb";
+
+            c_DB.Retrieve(_person.Id);
+        }
+
+        [TestMethod]
         public void TestRetrieveAPerson()
         {
             var _result = c_DB.Retrieve("person1");
