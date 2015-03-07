e38f6b51d4a993fedcd47bcf18394388d224e786
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-03-07 22:22:34 +0000

    FacialRecognition: added globals class to cleanly store displayed user details

---
+++ FacialRecognition/FacialRecognition/DatabaseUIGlobals.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FacialRecognition.Library.Models;
+
+namespace FacialRecognition
+{
+    public class DatabaseUIGlobals
+    {
+        public static Person DISPLAYED_USER { get; set; }
+        public static int DISPLAYED_IMAGE_INDEX { get; set; }
+    }
+}
