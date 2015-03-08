2f991f817933bf23c2f6f747dfa88a6c972ca9e5
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-03-08 16:32:34 +0000

    Octave: manual testing function updated to use new functions for facial classification

--- Octave/eigenfaces/testRecogniser.m
+++ Octave/eigenfaces/testRecogniser.m
@@ -1,24 +1,12 @@
-function [image distance idx] = testRecogniser(U,weights,averageFace,facePath)
+function [image idx] = testRecogniser(U,weights,averageFace,facePath)
   if(nargin != 4)
     usage("testRecogniser(U, weights, averageFace, facePath)");
   endif
   
   #Projecting New Face
   image = double(imread(facePath));
-  #"~/Desktop/yaleB01_P00A+000E+00.pgm"
-  
   image = reshape(image,rows(image) * columns(image),1);
 
   weightOfUnknownFace = projectFace(U,image,averageFace);
-
-  #Euclidian Distance
-  #Loop through weights and find smallest distance
-  #ED = norm(W - W, 2); #use 2-norm approach
-  
-  #ED from between unknown face and each face
-  for i=1:columns(weights)
-    ED(1,i) = norm(weights(:,i) - weightOfUnknownFace, 2);
-  endfor
-  
-  [distance idx] = min(ED);
+  idx = nearestMatchEuclideanDistance(weights, weightOfUnknownFace)
 endfunction
