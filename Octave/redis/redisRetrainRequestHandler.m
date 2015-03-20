0043558f2e0f4287b80108433ce1f306267947f4
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-03-20 19:44:34 +0000

    Octave: retrain function reading data and labels from Redis - correctly completes when 'no remaining data' indicator is encountered

--- Octave/redis/redisRetrainRequestHandler.m
+++ Octave/redis/redisRetrainRequestHandler.m
@@ -1,7 +1,40 @@
-function sessionData = redisRetrainRequestHandler()
-  if(nargin != 0)
-    usage("redisRetrainRequestHandler requires no parameters");
+function sessionData = redisRetrainRequestHandler(redisConnection)
+  if(nargin != 1)
+    usage("redisRetrainRequestHandler(redisConnection)");
   endif
   
+  # Constants
+  redisListLabels = "facial.database.labels";
+  redisListData = "facial.database.data";
+  noRemainingData = "50";
+  
+  # Read all data from the cache
+  faceLabels = {};
+  faces = [];
+  done = false;
+  
+  while(!done)
+    label = redisListLPOP(redisConnection, redisListLabels);
+    
+    # strcmp returns 1 (or true) if strings are equals
+    dataRemaining = strcmp(label, noRemainingData);
+    
+    if (!dataRemaining)
+      faceLabels = [faceLabels; label];
+      
+      # TODO
+      # Unmarshal the facial data
+      faceData = redisListLPOP(redisConnection, redisListData);
+      faces = [faces, faceData];
+    else
+      done = true;
+    endif
+  endwhile 
+  
+  sessionData.M = faces;
+  sessionData.labels = faceLabels;
+  
   # TODO
+  # Pass data and labels acquired from Redis to the trainRecogniser function
+  # sessionData = trainRecogniser();
 endfunction
