28e106fea51c2a71dd8b531227d005b76a9e3f50
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-03-22 14:22:42 +0000

    Octave: refactored recognition handler. Introduced constants, error handling and simplified code

--- Octave/redis/redisRecognitionRequestHandler.m
+++ Octave/redis/redisRecognitionRequestHandler.m
@@ -2,20 +2,23 @@ function sessionData = redisRecognitionRequestHandler(sessionData)
   if(nargin != 1)
     usage("redisRecognitionRequestHandler(sessionData)");
   endif
-
-  faceAsString = sessionData.requestData; #format "1,2,3.......,32256"
-  faceSplit = strsplit(faceAsString,",");
-  face = [];
-
-  for i=1:columns(faceSplit)
-    curPixel = faceSplit{i}; #char: 123
-    curPixel = str2double(curPixel);
-    face = [face;curPixel]; #construct column vector of facial image
-  endfor
   
-  #Result is face label
-  result = classifyAnUnknownFace(sessionData.U, sessionData.weights, face, sessionData.averageFace, sessionData.labels)
+  # Constants
+  RESPONSE_OK = 100;
+  RESPONSE_FAIL = 200;
   
-  sessionData.responseCode = 100;
-  sessionData.responseData = result;
+  # First, unmarshal the facial data.
+  # Then invoke the recognisers classification function which returns
+  # the label of the closest match.
+  
+  try
+    face = redisUnmarshalFacialData(sessionData.requestData)
+    labelOfClosestMatch = classifyAnUnknownFace(sessionData.U, sessionData.weights, face, sessionData.averageFace, sessionData.labels)
+  
+    sessionData.responseCode = RESPONSE_OK;
+    sessionData.responseData = labelOfClosestMatch;
+  catch
+    sessionData.responseCode = RESPONSE_FAIL;
+    sessionData.responseData = "An error occurred while handling the recognition request";
+  end_try_catch
 endfunction
