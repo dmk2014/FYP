e6efd8721a02ef40e689700856e91e28327c484b
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-03-29 15:04:04 +0100

    FacialRecogniton: added overloaded constructors for OctaveMessage

--- FacialRecognition/FacialRecognition.Library/Octave/OctaveMessage.cs
+++ FacialRecognition/FacialRecognition.Library/Octave/OctaveMessage.cs
@@ -7,6 +7,18 @@ namespace FacialRecognition.Library.Octave
         public int Code { set; get; }
         public String Data { set; get; }
 
+        public OctaveMessage()
+        {
+            this.Code = (int)OctaveMessageType.NoData;
+            this.Data = ((int)OctaveMessageType.NoData).ToString();
+        }
+
+        public OctaveMessage(int Code)
+        {
+            this.Code = Code;
+            this.Data = ((int)OctaveMessageType.NoData).ToString();
+        }
+
         public OctaveMessage(int Code, String Data)
         {
             this.Code = Code;
