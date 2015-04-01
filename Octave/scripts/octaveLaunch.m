b91d67b2b3dc6182110f54f3d4f1a20e7bc455c8
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-04-01 20:10:39 +0100

    Octave: launch script now initialises required global variables. Minor refactoring

--- Octave/scripts/octaveLaunch.m
+++ Octave/scripts/octaveLaunch.m
@@ -1,17 +1,17 @@
 cd("C:/FacialRecognition/startup");
 
-#Ensure FacialRecognition functions are accessible
-#This is all my Octave code
-#GenPath loads every .m file in the directory
+# Ensure FacialRecognition functions are accessible
+# This is all my Octave code
+# GenPath loads every .m file in the directory
 addpath(genpath("C:/FacialRecognition/FYP/Octave"));
 disp("Octave code loaded");
 
-#Ensure go-redis functions are accessible
+# Ensure go-redis functions are accessible
 addpath ("C:/FacialRecognition/octave-packages/go-redis/go-redis/");
 disp("Go-Redis package loaded");
 
-#Install and load required packages
-#Check if instrument-control is installed
+# Install and load required packages
+# Check if instrument-control is installed
 isInstalled = checkPackageInstalled("instrument-control");
 
 if (!isInstalled)
@@ -21,8 +21,13 @@ endif
 pkg load all;
 
 disp("Instrument control package loaded");
+
+# Initialise required global variables
+redisGlobals;
+
+disp("Redis globals initialised");
 disp("Octave startup complete");
 disp("Starting Redis listener with default settings...");
 
-#Start listening for Redis connections
+# Start listening for Redis connections
 redisListener(redisConnection())
