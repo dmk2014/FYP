9494445ad1265f89c6f634cf7eb59173437d0bcd
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-04-07 11:15:56 +0100

    Octave: bug fixed -> response code & data ordering

--- Octave/redis/redisSendResponse.m
+++ Octave/redis/redisSendResponse.m
@@ -2,7 +2,11 @@ function redisSendResponse(R, code, data)
   if (nargin != 3)
     usage("redisSendResponse(R, code, data)");
   endif
+  # Sends a response to Redis
   
-  redisSet(R, "facial.response.code", code);
+  # Transactionality is not supported by go-redis, the "EXEC" command will timeout.
+  # Therefore, the data key is set before the code key.
+  # This prevents an issue where calling clients were retrieving the data key before it had a value.
   redisSet(R, "facial.response.data", data);
+  redisSet(R, "facial.response.code", code);
 endfunction
