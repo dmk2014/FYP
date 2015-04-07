2f2eee0cbf12faac3980cf804ba3b9a96dd8e081
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-04-07 13:40:34 +0100

    FacialRecognition: recognition interface

--- FacialRecognition/FacialRecognition.Library/Core/IFacialRecogniser.cs
+++ FacialRecognition/FacialRecognition.Library/Core/IFacialRecogniser.cs
@@ -1,10 +1,14 @@
 ﻿using FacialRecognition.Library.Models;
 using System.Drawing;
+using System.Collections.Generic;
 
 namespace FacialRecognition.Library.Core
 {
     public interface IFacialRecogniser
     {
         Person ClassifyFace(Image facialImage);
+        bool SaveSession();
+        bool ReloadSession();
+        bool RetrainRecogniser(List<Person> people);
     }
 }
