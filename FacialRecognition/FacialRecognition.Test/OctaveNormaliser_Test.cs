af672492dd1cc686335f826649ff1e05a38a013b
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-04-11 00:15:27 +0100

    FacialRecognition: testing Octave normaliser

---
+++ FacialRecognition/FacialRecognition.Test/OctaveNormaliser_Test.cs
@@ -0,0 +1,23 @@
+﻿using FacialRecognition.Library.Octave;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FacialRecognition.Test
+{
+    [TestClass]
+    public class OctaveNormaliser_Test
+    {
+        [TestMethod]
+        public void TestNormaliseAnImage()
+        {
+            var normaliser = new OctaveNormaliser();
+            var sourceImage = Properties.Resources.FacialImage; 
+            var normalisationWidth = 168;
+            var normalisationHeight = 192;
+
+            var result = normaliser.NormaliseImage(sourceImage, normalisationWidth, normalisationHeight);
+
+            Assert.AreEqual(normalisationWidth, result.Width);
+            Assert.AreEqual(normalisationHeight, result.Height);
+        }
+    }
+}
