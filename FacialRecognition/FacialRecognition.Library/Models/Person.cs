445fa08d53b37737f1bf474eabfb5a56793231ba
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-04-13 19:43:19 +0100

    FacialRecognition: documented person models

--- FacialRecognition/FacialRecognition.Library/Models/Person.cs
+++ FacialRecognition/FacialRecognition.Library/Models/Person.cs
@@ -6,11 +6,29 @@ namespace FacialRecognition.Library.Models
 {
     public class Person
     {
+        /// <summary>
+        /// Get or set the persons Id.
+        /// </summary>
         public String Id { get; set; }
+
+        /// <summary>
+        /// Get or set the persons forename.
+        /// </summary>
         public String Forename { get; set; }
+
+        /// <summary>
+        /// Get or set the persons surname.
+        /// </summary>
         public String Surname { get; set; }
+
+        /// <summary>
+        /// Get or set the List of images associated with the person.
+        /// </summary>
         public List<Image> Images { get; set; }
 
+        /// <summary>
+        /// Create a new person object.
+        /// </summary>
         public Person()
         {
             this.Images = new List<Image>();
--- FacialRecognition/FacialRecognition.Library/Models/PersonCouchDB.cs
+++ FacialRecognition/FacialRecognition.Library/Models/PersonCouchDB.cs
@@ -4,6 +4,9 @@ namespace FacialRecognition.Library.Models
 {
     public class PersonCouchDB : Person, ICouchDocument
     {
+        /// <summary>
+        /// Get or set the persons revision number.
+        /// </summary>
         public string Rev { get; set; }
     }
 }
