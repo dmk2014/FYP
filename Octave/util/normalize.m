73022135a0f42e7c372fd2815b629a9daed3c12e
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-04-14 15:16:25 +0100

    Octave: documented normalise function

--- Octave/util/normalize.m
+++ Octave/util/normalize.m
@@ -1,9 +1,21 @@
 function result = normalize(M, low, high)
+  % normalise - ensure all values of the matrix M are within the range [low - high]
+  %
+  % Inputs:
+  %    M - the matrix to normalise
+  %    low - the lower bound to normalise to
+  %    high - the upper bound to normalise to
+  %
+  % Outputs:
+  %    result - a normalised matrix
+
+  % Author: Darren Keane
+  % Institute of Technology, Tralee
+  % email: darren.m.keane@students.ittralee.ie
+  
   if(nargin != 3)
     usage("normalise(M, low, high)");
   endif
-  
-  % Normalise all values of M to within the range [low - high]
   
   minimumValue = min(M);
   maximumValue = max(M);
