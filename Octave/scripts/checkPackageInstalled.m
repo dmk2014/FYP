25cf2425d7ffd9e0dbc4006d7923091e6b77ac88
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-04-14 15:13:44 +0100

    Octave: documented function to check if a package is already installed

--- Octave/scripts/checkPackageInstalled.m
+++ Octave/scripts/checkPackageInstalled.m
@@ -1,4 +1,16 @@
 function result = checkPackageInstalled(pkgName)
+  % checkPackageInstalled - checks if the specified package is already installed
+  %
+  % Inputs:
+  %    pkgName - the name of the package
+  %
+  % Outputs:
+  %    result - boolean indicating if the package is installed
+
+  % Author: Darren Keane
+  % Institute of Technology, Tralee
+  % email: darren.m.keane@students.ittralee.ie
+  
   if(nargin != 1)
     usage("checkPackageInstalled(pkgName)");
   endif
