ffc342a8b1f6cd99c00d2236f851a07aef921845
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-04-15 16:25:17 +0100

    FacialRecognition: testing OctaveNormaliser

--- FacialRecognition/FacialRecognition.Test/OctaveNormaliser_Test.cs
+++ FacialRecognition/FacialRecognition.Test/OctaveNormaliser_Test.cs
@@ -1,23 +1,58 @@
 ﻿using FacialRecognition.Library.Octave;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
 
 namespace FacialRecognition.Test
 {
     [TestClass]
     public class OctaveNormaliser_Test
     {
-        [TestMethod]
-        public void TestNormaliseAnImage()
+        OctaveNormaliser Normaliser;
+        Bitmap TestImage;
+
+        [TestInitialize]
+        public void InitializeTest()
         {
-            var normaliser = new OctaveNormaliser();
-            var sourceImage = Properties.Resources.FacialImage; 
+            this.Normaliser = new OctaveNormaliser();
+            this.TestImage = Properties.Resources.FacialImage; 
+        }
+
+        [TestMethod]
+        public void TestNormaliseAnImageUsingTemplateMethod()
+        {
             var normalisationWidth = 168;
             var normalisationHeight = 192;
 
-            var result = normaliser.NormaliseImage(sourceImage, normalisationWidth, normalisationHeight);
+            var result = this.Normaliser.NormaliseImage(this.TestImage, normalisationWidth, normalisationHeight);
             
             Assert.AreEqual(normalisationWidth, result.Width);
             Assert.AreEqual(normalisationHeight, result.Height);
+        }
+
+        [TestMethod]
+        public void TestResizeAnImage()
+        {
+            var resizeWidth = 50;
+            var resizeHeight = 50;
+
+            var result = this.Normaliser.Resize(this.TestImage, resizeWidth, resizeHeight);
+
+            Assert.AreEqual(resizeWidth, result.Width);
+            Assert.AreEqual(resizeHeight, result.Height);
+        }
+
+        [TestMethod]
+        public void TestSetColormapOfImage()
+        {
+            // No exceptions indicate success
+            var result = this.Normaliser.SetColormap(this.TestImage);
+
+            // Check a random pixel to ensure all components contain the same value
+            var resultAsBitmap = new Bitmap(result);
+            var colorOfPixel = resultAsBitmap.GetPixel(1, 1);
+
+            Assert.AreEqual(colorOfPixel.G, colorOfPixel.R);
+            Assert.AreEqual(colorOfPixel.R, colorOfPixel.B);
         }
     }
 }
