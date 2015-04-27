b947cd2b2bc4c0d641a9978c90fff962073a223d
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-04-27 22:34:20 +0100

    Octave: added script to excecute all tests. IO tests implemented

---
+++ Octave/test/runAllTests.m
@@ -0,0 +1,27 @@
+% runAllTests - This script will execute all implemented tests
+
+% Author: Darren Keane
+% Institute of Technology, Tralee
+% email: darren.m.keane@students.ittralee.ie
+
+% ___Prepare For Testing___
+
+disp("Initialising application for testing...");
+
+% Ensure all testable functions are added to the load path
+addpath(genpath("C:/FacialRecognition/FYP/Octave"));
+disp("Octave code added to load path");
+
+% Ensure go-redis functions are accessible
+addpath ("C:/FacialRecognition/octave-packages/go-redis/go-redis/");
+disp("Go-Redis package loaded");
+
+% Ensure all packages are loaded
+pkg load all;
+disp("All installed packages loaded");
+disp("...COMPLETE");
+
+% ___Execute Test Scripts___
+
+disp("\nRunning IO Tests...");
+test testIO;
---
+++ Octave/test/testIO.m
@@ -0,0 +1,23 @@
+% testIO - This script will execute tests on IO functions
+
+% Author: Darren Keane
+% Institute of Technology, Tralee
+% email: darren.m.keane@students.ittralee.ie
+
+% ___Test Load Yale Training Database___
+
+%!test
+%! yaleFaces = loadYaleTrainingDatabase();
+%! 
+%! expectedRows = 32256;
+%! expectedColumns = 2432;
+%!
+%! assert(rows(yaleFaces), expectedRows)
+%! assert(columns(yaleFaces), expectedColumns);
+
+
+% ___Test Load Matrix Data Fails For Non-Existing File___
+
+%!test
+%! functionCall = "loadMatrixData(\"this_file_does_not_exist\")";
+%! fail(functionCall);
