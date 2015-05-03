fc867bf1bb61abaffbfd896930024af97352b19f
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-05-03 14:29:23 +0100

    FacialRecognition: minor change to detection - largest rectangle used for extraction

--- FacialRecognition/FacialRecognition/Controllers/DetectionController.cs
+++ FacialRecognition/FacialRecognition/Controllers/DetectionController.cs
@@ -1,7 +1,8 @@
 ﻿using FacialRecognition.Globals;
+using FacialRecognition.Util;
+using System;
 using System.Drawing;
 using System.Windows.Forms;
-using FacialRecognition.Util;
 
 namespace FacialRecognition.Controllers
 {
@@ -36,11 +37,40 @@ namespace FacialRecognition.Controllers
 
         public Image ExtractFacialImage(Image sourceImage, Rectangle[] faceLocations)
         {
-            var image = new Bitmap(sourceImage);
+            if (faceLocations.Length > 0)
+            {
+                var faceIndexToExtract = this.GetIndexOfLargestFace(faceLocations);
+                var image = new Bitmap(sourceImage);
 
-            var face = image.Clone(faceLocations[0], System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+                var face = image.Clone(faceLocations[faceIndexToExtract], System.Drawing.Imaging.PixelFormat.Format32bppRgb);
 
-            return face;
+                return face;
+            }
+            else
+            {
+                throw new Exception("No facial images could be extracted - ensure that a face has been captured");
+            }
+        }
+
+        private int GetIndexOfLargestFace(Rectangle[] faceLocations)
+        {
+            int indexLargestFace = 0;
+            int areaLargestFace = 0;
+
+            for (int i = 0; i < faceLocations.Length; i++ )
+            {
+                int width = faceLocations[i].Width;
+                int height = faceLocations[i].Height;
+                int areaFace = width * height;
+
+                if (areaFace > areaLargestFace)
+                {
+                    indexLargestFace = i;
+                    areaLargestFace = areaFace;
+                }
+            }
+
+            return indexLargestFace;
         }
     }
 }
